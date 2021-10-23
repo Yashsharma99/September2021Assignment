6e005838b87b5eba8a264dc9d3022ebbd5925ae9
Yashsharma99 <40380694+Yashsharma99@users.noreply.github.com>
2021-10-23 16:33:45 +0530

    first commit
    asshin1

---
+++ src/main/java/com/nagarro/devopsAssignment1/controller/AdditionController.java
@@ -0,0 +1,29 @@
+package com.nagarro.devopsAssignment1.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+
+public class AdditionController {
+	@RequestMapping(value="/add")
+	public ModelAndView add(HttpServletRequest req, HttpServletResponse resp) {
+
+		ModelAndView mv = new ModelAndView();
+		int a = Integer.parseInt(req.getParameter("num1"));
+		int b = Integer.parseInt(req.getParameter("num2"));
+		int sum = a + b;
+		mv.addObject("result", sum);
+		mv.setViewName("addResult");
+
+		return mv;
+
+	}
+
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<form action = "add"> 
+   Num1 : <input type ="number" name="num1"><br>
+   Num2 : <input type ="number" name="num2"><br>
+   <button type ="submit">Submit</button>
+</from> 
+</body>
+</html>
